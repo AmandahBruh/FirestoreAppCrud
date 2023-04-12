628db8cb58d4abe8ddff39d3944a696215c9a5f1
Amanda <eunumseioqpordeemail@gmail.com>
2023-04-12 14:26:54 -0300

    socorroooooo

--- src/screen/CreateUser.jsx
+++ src/screen/CreateUser.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import { View } from "react-native";
 import { Text, TextInput } from "react-native-paper";
 import { styles } from "../utils/styles";
 
 export default function CreateUser() {
+  const [nome, setNome] = useState("");
+
   return (
     <View style={styles.container}>
       <Text>Cadastrar Pessoa</Text>
       <View>
-        <TextInput label={"Nome"} mode="outlined">
-        Nome
+        <TextInput
+          label={"Nome"}
+          mode="outlined"
+          placeholder="Digite seu nome..."
+          onChangeText={setNome}>
+          Nome
         </TextInput>
-        <Button>a
-        </Button>
-      </View> 
+        <Button>Cadastrar</Button>
+      </View>
     </View>
   );
 }
